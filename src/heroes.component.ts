8b5bc9abe844bce37b0f9c0f2946c0a1409c1507
Adam Hakowski <adamhakowski@MacBook-Pro-Adam.local>
2016-08-16 19:49:58 +0200

    not working

--- src/heroes.component.ts
+++ src/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -79,7 +80,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private router: Router) { }
   
   getHeroes(){
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -90,5 +91,10 @@ export class HeroesComponent implements OnInit {
   }
 
   onSelect(hero: Hero){ this.selectedHero = hero; }
+
+  gotoDetail() {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
  }
 
